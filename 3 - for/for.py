dc57f678b7dc823c541f40bd51ff41146320af24
Misterio Rojo <aloxfloyd@gmail.com>
2022-09-21 02:25:14 +0200

    Tutorial bucle for

---
+++ 3 - for/for.py
@@ -0,0 +1,20 @@
+# bucle for basico
+
+# bucle usando un int usando input
+n = int(input("Numero de repeticiones: "))
+for i in range(n):
+    print("Repeticion: ", i + 1)
+
+# bucle de un String
+t = input("Di una palabra: ")
+for i in t:
+    print(i)
+
+# Bucle for usando range
+for i in range(15):
+    print("Repeticiones: ", i + 1)
+
+# Bucle for mediante un array
+dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+for i in dias:
+    print("Dia: ", i)
