b4fe16e2ae39b066c1e7d98c2cb1e68921ab6d27
AlejandroRodriguezM <aloxfloyd@gmail.com>
2022-10-03 21:02:15 +0200

    5 - funciones

---
+++ 5 - funciones/funciones.py
@@ -0,0 +1,12 @@
+def division(num1, num2):
+    try:
+        num3 = num1 / num2
+        return num3
+    except Exception:
+        print("No se puede dividir entre 0")
+
+
+if __name__ == '__main__':
+    num1 = int(input("Numero 1: "))
+    num2 = int(input("Numero 2: "))
+    print(division(num1, num2))
