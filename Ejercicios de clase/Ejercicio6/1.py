9bc2fdf8850ddb96033260c83f324fcb6a6ae236
AlejandroRM-Torre <aloxfloyd@gmail.com>
2022-11-16 14:20:13 +0100

    Cambios en ejercicio 6

--- Ejercicios de clase/Ejercicio6/1.py
+++ Ejercicios de clase/Ejercicio6/1.py
@@ -3,13 +3,13 @@
 # programa será mostrar las sumas parciales de las filas y las columnas, como si fuera una hoja de
 # cálculo. La suma de todos los números aparecerá en la esquina inferior derecha.
 
-
 # Funcion que sirve para pedir los numeros
 def pedirNumeros():
+    global array
     try:
         for i in range(4):
             for j in range(5):
-                print("Columna:", i+1, "Fila:", j+1)
+                print("Columna:", (i + 1), "Fila:", (j + 1))
                 array[i][j] = int(input("Introduce un numero: "))
         return array
     except ValueError:
@@ -17,43 +17,44 @@ def pedirNumeros():
         raise SystemExit
 
 
-# Funcion que suma las filas
+# Funcion que suma las filas devuelve el valor de cada fila individualmente
 def sumaFilas(array):
     for i in range(4):
+        suma = 0
         for j in range(5):
-            suma_filas[i][0] += array[i][j]
-    return suma_filas
+            suma += array[i][j]
+    print(suma, end="\t")
 
 
 # Funcion que suma las columnas
 def sumaColumnas(array):
     for i in range(5):
+        suma = 0
         for j in range(4):
-            suma_columnas[0][i] += array[j][i]
-    return suma_columnas
+            suma += array[j][i]
+    print(suma, end="\t")
 
 
 # Muestro el array
 def mostrarArray(array):
     for i in range(4):
-        for j in range(6):
-            if j == 5:
-                suma_filas[i][0] = sumaFilas(array)[i][0]
-                print(suma_filas[i][0], end="\t")
-            else:
-                array[i][j] = pedirNumeros()[i][j]
-                print(array[i][j], end="\t")
+        for j in range(5):
+            print(array[i][j], end="\t")
+        sumaFilas(array)
         print()
 
 
 # Muestro las sumas de las columnas
-def mostrarSumaColumnas():
-    for i in range(6):
-        if i == 5:
-            print(sumaTotal(), end="\t")
-        else:
-            suma_columnas[0][i] = sumaColumnas(array)[0][i]
-            print(suma_columnas[0][i], end="\t")
+def mostrarSumaColumnas(array):
+    suma = 0
+    for i in range(5):
+        total_columnas = 0
+        for j in range(4):
+            total_columnas += array[j][i]
+            suma += array[j][i]
+
+        print(total_columnas, end="\t")
+    print(suma, end="\t")
     print()
 
 
@@ -67,7 +68,7 @@ def sumaTotal():
 
 
 # Creo un array de 4 filas y 5 columnas
-array = [[0 for x in range(6)] for y in range(4)]
+array = [[0 for x in range(5)] for y in range(4)]
 # Creo un array de 4 filas y 1 columna para guardar las sumas de las filas
 suma_filas = [[0] for x in range(4)]
 # Creo un array de 1 fila y 5 columnas para guardar las sumas de las columnas
@@ -75,8 +76,9 @@ suma_columnas = [[0 for x in range(5)]]
 
 
 def main():
+    pedirNumeros()
     mostrarArray(array)
-    mostrarSumaColumnas()
+    mostrarSumaColumnas(array)
     sumaTotal()
 
 
